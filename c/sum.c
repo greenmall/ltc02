c28c5ffc113e8ef0efa004bf8bd8d543ff731db2
Green <zelennata@ya.ru>
2019-03-17 16:42:25 +0200

    sum

---
+++ c/sum.c
@@ -0,0 +1,9 @@
+#include "stdio.h"
+
+int sum(int a, int b) {
+return a + b;
+}
+
+int main() {
+printf("sum=%d\n", sum(1, 2));
+}
---
+++ js/sum.js
@@ -0,0 +1,6 @@
+function sum(a, b) {
+    return a + b
+}
+
+let this_sum = sum(1, 2)
+console.log(this_sum)
