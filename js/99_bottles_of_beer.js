83f414b8e47fb79fcee14982630b95073174142b
greenmall <zelennata@ya.ru>
2019-04-01 14:27:31 +0300

    Update 99_bottles_of_beer.js

--- js/99_bottles_of_beer.js
+++ js/99_bottles_of_beer.js
@@ -4,9 +4,9 @@ if(count > 1) {
   
   minus = count - 1;
 
-  console.log("count + "bottles of beer on the wall,\n"
-   + count + "bottles of beer. \n"  +
-"Take one down, pass it around,\n"
+  console.log("count + "bottles of beer on the wall,"
+   + count + "bottles of beer.
+Take one down, pass it around,\n"
 + minus + "bottles of beer on the wall...\n"");
 
 beersong(minus);
