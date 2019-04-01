6a70ffc110d1e93ac2b4014efc5680000e2c4b63
greenmall <zelennata@ya.ru>
2019-04-01 14:20:46 +0300

    recursion

---
+++ js/99_bottles_of_beer.js
@@ -0,0 +1,25 @@
+function beersong(count){
+
+if(count > 1) {
+  
+  minus = count - 1;
+
+  console.log("count + "bottles of beer on the wall,\n"
+   + count + "bottles of beer. \n
+Take one down, pass it around,\n"
++ minus + "bottles of beer on the wall...\n"");
+
+beersong(minus);
+}
+
+else {console.log("If that one bottle should happen to fall,\n
+  what a waste of alcohol!\n")
+}
+else  {console.log("No more bottles of beer on the wall,\n
+no more bottles of beer.\n
+We've taken them down and passed them around\n
+now we're drunk and passed out!\n");
+}
+}
+
+beersong(99);
