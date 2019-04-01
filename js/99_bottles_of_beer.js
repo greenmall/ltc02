cd5d8841d32a23b676bd1bd1fb25c714da8f1184
greenmall <zelennata@ya.ru>
2019-04-01 14:26:07 +0300

    Update 99_bottles_of_beer.js

--- js/99_bottles_of_beer.js
+++ js/99_bottles_of_beer.js
@@ -5,20 +5,20 @@ if(count > 1) {
   minus = count - 1;
 
   console.log("count + "bottles of beer on the wall,\n"
-   + count + "bottles of beer. \n
-Take one down, pass it around,\n"
+   + count + "bottles of beer. \n" +
+"Take one down, pass it around,\n"
 + minus + "bottles of beer on the wall...\n"");
 
 beersong(minus);
 }
 
-else {console.log("If that one bottle should happen to fall,\n
-  what a waste of alcohol!\n")
+else {console.log("If that one bottle should happen to fall,\n" +
+  "what a waste of alcohol!\n")
 }
-else  {console.log("No more bottles of beer on the wall, \n
-No more bottles of beer. \n
-We've taken them down and passed them around \n
-Now we're drunk and passed out! \n");
+else  {console.log("No more bottles of beer on the wall, \n" +
+"No more bottles of beer. \n" +
+"We've taken them down and passed them around \n" +
+"Now we're drunk and passed out! \n");
 }
 }
 
